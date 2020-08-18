f639648b4c9e9c5b3cc30bbe9352defac342562b
aivarsyo <aiva0071@stud.kea.dk>
2020-08-18 14:54:07 +0200

    cloned and moved

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import "./styles/main.scss";
 import { gsap } from "gsap";
 
-const oneFifthOfTheScreen = window.innerWidth/5;
-const slideCount = document.querySelectorAll(".carousel__slide").length;
+
+
+
+const cloneAwards = () => {
+    const allAwards = document.querySelector(".glide__slides")
+    var cln = allAwards.cloneNode(true)
+    document.querySelector(".glide__container").appendChild(cln)
+
+    console.log("Cloned!")
+}
+
+
 
 function carouselAnim(){
 
-    gsap.set(".carousel__slide", {
-        x: (i) => i * oneFifthOfTheScreen
-      });
-      
-      
-      gsap.to(".carousel__slide", {
-        duration: 10,
-        ease: "none",
-        x: `+=${oneFifthOfTheScreen*slideCount}`,
-        modifiers: {
-          x: gsap.utils.unitize(x => parseFloat(x) % (oneFifthOfTheScreen*slideCount))
-        },
-        repeat: -1
-      });
-
-      document.querySelector(".carousel").style.width = oneFifthOfTheScreen*slideCount+"px";
+    gsap.to(".glide__container", 5, {
+x:"-100vw",
+repeat: -1,
+ease: "none"
+    })
 }
+
+
 
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init(){
+    cloneAwards();
     carouselAnim();
 }
